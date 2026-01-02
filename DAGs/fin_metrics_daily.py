51d298e1ca1b1c4f92bba4caa079e914d85427cb
Сергей Рябинкин <164728072+Coxxx163@users.noreply.github.com>
2026-01-02 21:45:23 +0400

    Create fin_metrics_daily.py

---
+++ DAGs/fin_metrics_daily.py
@@ -0,0 +1,25 @@
+from airflow import DAG 
+from airflow.operators.python_operator import PythonOperator 
+from datetime import datetime 
+from functions import execute_sql_script    
+
+OWNER = "{{ OWNER }}" #обеспечивает уникальность дагов по ученикам для деплоя
+
+with DAG(
+          dag_id=f'fin_metrics_daily_{OWNER}', #меняет название дага до _dag
+          start_date = datetime(2025, 11, 30),
+          schedule_interval='50 11 * * *', #меняем расписание запуска дага
+          catchup=False,
+          tags=[OWNER],
+          default_args={
+                    "owner": OWNER
+          }
+) as dag:
+
+          fin_metrics_daily = PythonOperator( #меняем название задачи
+                  task_id = 'fin_metrics_daily', #меняем название задачи
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/fin_metrics_daily'} #меняем название sql скрипта
+          )
+
+fin_metrics_daily #меняем название задачи
